a3e05f29eed2c2e3d67010884ab30abf403cec75
Robert Odhiambo <odhizrober254@gmail.com>
2023-09-07 19:55:25 +0000

    command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+args = sys.argv[1:]
+args_len = len(args)
+if args_len == 0:
+    print("{} arguments.".format(args_len))
+else:
+    if args_len == 1:
+        print("{} argument:".format(args_len))
+    else:
+        print("{} arguments:".format(args_len))
+    for i in range(args_len):
+        print("{}: {}".format(i + 1, args[i]))
