323dc277dc1496930a37e813e55ebaa350fc6857
Robert Odhiambo <odhizrober254@gmail.com>
2023-09-10 10:40:41 +0000

    divisibility test

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-#Finding the maximum value in a list
+# Finding the maximum value in a list
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    #Set first value of list to maximum value
+    # Set first value of list to maximum value
     maximum = my_list[0]
-    #Iterating through the list
+    # Iterating through the list
     for i in range(1, len(my_list)):
-        #Check if the value at i is greater than the current maximum value and swap
+        # Check if the value at i is greater than
+        # the current maximum value and swap
         if my_list[i] > maximum:
             maximum = my_list[i]
     return (maximum)
