3c2604a800054f43163fb373bcc630c042b10520
Robert Odhiambo <odhizrober254@gmail.com>
2023-09-10 10:44:40 +0000

    output for divisibilty test

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,11 +2,12 @@
 def divisible_by_2(my_list=[]):
     if not my_list:
         return None
-    list_len = len(my_list)
-    for i in range(list_len):
-        if (my_list[i] % 2) == 0:
-            my_list[i] = True
-        else:
-            my_list[i] = False
 
-    return (my_list)
+    result = []
+    for num in my_list:
+        if (num % 2) == 0:
+            result.append(True)
+        else:
+            result.append(False)
+
+    return (result)
