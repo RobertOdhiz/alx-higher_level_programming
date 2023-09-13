f674981e1f50dab02842579b9c42c68ecc66692c
Robert Odhiambo <odhizrober254@gmail.com>
2023-09-13 19:34:03 +0000

    Simple Square matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    copy = [row[:] for row in matrix]
+    for i in range(len(copy)):
+        for j in range(len(copy[i])):
+                copy[i][j] = copy[i][j] ** 2
+
+    return copy
