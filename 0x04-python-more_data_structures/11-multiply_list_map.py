c2654a1497fc9a68bceb41263b365e6b74a53a03
Robert Odhiambo <odhizrober254@gmail.com>
2023-09-13 23:16:16 +0000

    mul

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def multiply_by_number(x, number):
-    return x * number
-
-def multiply_list_map(my_list, number):
-    result = list(map(multiply_by_number, my_list, [number] * len(my_list)))
-    return result
+def multiply_list_map(my_list, number=0):
+    mul = list(map(lambda x: x * number, my_list))
+    return mul
