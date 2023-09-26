7d0d558634def2693e5f04d5b9c57c9925092c7f
Robert Odhiambo <odhizrober254@gmail.com>
2023-09-26 10:31:51 +0000

    private size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Rep a square"""
+
+    def __init__(self, size):
+        """Initializing a new square
+
+        args:
+	    size(int): size of a new square
+        """
+        self.__size = size
