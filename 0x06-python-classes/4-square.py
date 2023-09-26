99114013355f094564991a76c8c1db09dcfc652b
Robert Odhiambo <odhizrober254@gmail.com>
2023-09-26 11:02:09 +0000

    mod: area of a square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-"""Define a class Square"""
+"""Define a class Square."""
 
 
 class Square:
-    """Rep a square"""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new Square.
+        """Initialize a new square.
 
-        args:
-            size (int): size of the new square.
+        Args:
+            size (int): The size of the new square.
         """
         self.size = size
 
-        @property
-        def size(self):
-            """Get current size of the square"""
-            return (self.__size)
+    @property
+    def size(self):
+        """Set the current size of the square."""
+        return (self.__size)
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
-        """Returns area of a square"""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
