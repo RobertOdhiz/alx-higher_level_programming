033b6f496d554ab136a1c2171ccb3d562f7c9caa
Robert Odhiambo <odhizrober254@gmail.com>
2023-10-02 13:56:29 +0000

    Rectangle class attributes with setters and getters

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Definition of an empty rectangle class"""
+
+
+class Rectangle:
+    """
+    Beginning of class definition
+    """
+    def __init__(self, width=0, height=0):
+        """Initiaalizing Rectangle vars"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter for private instance attribute width"""
+        return self.__width
+
+    @width_setter
+    def width(self, value):
+        """Setter for private instance attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for private instance att height"""
+        return self.__height
+
+    @height_setter
+    def height(self, value):
+        """Setter for private att height"""
+        @property
+    def width(self):
+        """Getter for private instance attribute width"""
+        return self.__width
+
+    @width_setter
+    def width(self, value):
+        """Setter for private instance attribute width"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
