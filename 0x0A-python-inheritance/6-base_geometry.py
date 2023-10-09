e51bf5e40922b402b39818ec32ab560272e90626
Robert Odhiambo <odhizrober254@gmail.com>
2023-10-09 16:40:48 +0000

    undef func area()

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Initializing class method BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ Empty Class """
+    def area(self):
+        """ Undefined function area() """
+        raise Exception("area() is not implemented")
