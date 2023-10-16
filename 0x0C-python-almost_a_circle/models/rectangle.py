20a236bc965660fb9a35ac9c951de712eac4a60b
Robert Odhiambo <odhizrober254@gmail.com>
2023-10-16 14:59:29 +0000

    mod func display()

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,7 +77,10 @@ class Rectangle(Base):
 
     def display(self):
         """ Prints in stdout the rectangle instance in # """
+        for r in range(self.__y):
+            print("")
         for i in range(self.__height):
+            print(" " * self.__x, end="")
             print("#" * self.__width)
 
     def __str__(self):
