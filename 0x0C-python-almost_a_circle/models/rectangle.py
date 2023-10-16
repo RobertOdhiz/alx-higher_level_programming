48c2d23fa31703d850c12e74dbeb1dbc0c4cc548
Robert Odhiambo <odhizrober254@gmail.com>
2023-10-16 15:07:41 +0000

    added: update()

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,6 +83,19 @@ class Rectangle(Base):
             print(" " * self.__x, end="")
             print("#" * self.__width)
 
+    def update(self, *args):
+        """ Method that assigns an argument to each attribute """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+
     def __str__(self):
         """ Returns the string representation of Rectangle """
         string = "[Rectangle] ({}) {}/{} - {}/{}".format(
