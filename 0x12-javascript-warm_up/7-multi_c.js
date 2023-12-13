eb81ba2a227cc2519517f26695c7a6dc771cd3a4
Robert Odhiambo <odhizrober254@gmail.com>
2023-12-13 07:23:40 +0000

    multi_c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-// prints x times "C is fun"
+// prints 3 lines: (like 1-multi_languages.js) but by
+// using an array of string and a loop
 
-const state = "C is fun"
+const lang = 'C is fun';
+
 if (isNaN(process.argv[2])) {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log(state);
+    console.log(lang);
   }
 }
