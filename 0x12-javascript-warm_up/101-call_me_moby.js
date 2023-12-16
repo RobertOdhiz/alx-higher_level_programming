ab1179533e98453039a04078af62f49e9a6bb0cd
Robert Odhiambo <odhizrober254@gmail.com>
2023-12-16 16:50:45 +0000

    callme Moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // executes x times a function
 
-exports.theFunction = function (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
