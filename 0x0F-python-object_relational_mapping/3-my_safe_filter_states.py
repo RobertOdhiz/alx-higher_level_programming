bbaac1544a45a5168a6edabbf6547a1b937760ed
Robert Odhiambo <odhizrober254@gmail.com>
2023-12-17 14:11:55 +0300

    Done

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that takes in arguments and displays all values 
+Script that takes in arguments and displays all values
 in the states table of hbtn_0e_0_usa tables where name matches
 the argument
 """
