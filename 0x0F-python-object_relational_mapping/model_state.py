99777fd03f80c0eb7f529df8a801d7c85a58fa91
Robert Odhiambo <odhizrober254@gmail.com>
2023-12-17 16:52:52 +0300

    fetch all

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Module with class definition of a State and an instance Base = declarative_base()
+Module with class definition of a State and an instance
+Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
