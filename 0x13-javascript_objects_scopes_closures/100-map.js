312a6ccc97fed89cd26bbbf8a530a020e102f692
Robert Odhiambo <odhizrober254@gmail.com>
2023-12-20 00:45:51 +0300

    map

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// imports an array and computes a new array
+
+const original = require('./100-data').list;
+console.log(original);
+const mappedList = original.map ((e, index) => {
+    return (e * index);
+});
+console.log(mappedList);
