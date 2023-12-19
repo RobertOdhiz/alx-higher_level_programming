15f976dbac7fb3d0d190708690735997c1f2169f
Robert Odhiambo <odhizrober254@gmail.com>
2023-12-20 00:09:46 +0300

    rotate(), double()

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -14,5 +14,16 @@ class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.height = 2 * this.height;
+    this.width = 2 * this.width;
+  }
 }
 module.exports = Rectangle;
