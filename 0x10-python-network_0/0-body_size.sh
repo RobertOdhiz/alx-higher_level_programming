a3186f90fc63be4817062eb5d0bda476242e70a9
Robert Odhiambo <odhizrober254@gmail.com>
2024-01-05 19:04:40 +0300

    body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# takes in a URL, sends a request to that URL
+# and displays the size of the body of the response
 curl -s -w %{size_download}"\n" "$1" -o /dev/null
