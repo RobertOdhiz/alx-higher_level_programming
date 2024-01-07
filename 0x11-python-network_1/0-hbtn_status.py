b9f0ef489ee073b1fd6dc4511954a649bdb9eed4
Robert Odhiambo <odhizrober254@gmail.com>
2024-01-07 18:08:51 +0300

    response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,12 +5,14 @@ Python script
 
 
 if __name__ == '__main__':
-    from urllib.request import Request, urlopen
+    from urllib.request import urlopen
 
-    req = Request('https://intranet.hbtn.io/status')
-    with urlopen(req) as f:
-        the_page = f.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(the_page)))
-        print('\t- content: {}'.format(the_page))
-        print('\t- utf8 content: {}'.format(the_page.decode('utf8')))
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urlopen(url) as response:
+        content = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode('utf-8')))
