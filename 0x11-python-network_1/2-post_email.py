3342cef77416e5f53b9242cb2a0e1af65b1f9532
Robert Odhiambo <odhizrober254@gmail.com>
2024-01-07 18:36:57 +0300

    Post

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,14 +8,11 @@ if __name__ == "__main__":
     from sys import argv
     import urllib.parse as up
 
-
     url = argv[1]
     values = {'email': argv[2]}
     data = up.urlencode(values).encode('utf-8')
-    
-    req = Request(url, data, method='POST')
 
+    req = Request(url, data, method='POST')
     with urlopen(req) as response:
         body = response.read()
-
     print(body.decode('utf-8'))
