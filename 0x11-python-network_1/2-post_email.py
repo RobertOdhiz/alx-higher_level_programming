06774985464a810c20a85b190c77f93a09a8b0d2
Robert Odhiambo <odhizrober254@gmail.com>
2024-01-07 18:34:53 +0300

    Post

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+POST script
+"""
+
+if __name__ == "__main__":
+    from urllib.request import Request, urlopen
+    from sys import argv
+    import urllib.parse as up
+
+
+    url = argv[1]
+    values = {'email': argv[2]}
+    data = up.urlencode(values).encode('utf-8')
+    
+    req = Request(url, data, method='POST')
+
+    with urlopen(req) as response:
+        body = response.read()
+
+    print(body.decode('utf-8'))
