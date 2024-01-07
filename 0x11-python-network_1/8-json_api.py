6872185cf722eb78e3d433f3e374618eda43ebb9
Robert Odhiambo <odhizrober254@gmail.com>
2024-01-07 20:08:14 +0300

    JSON api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {'q': q}
+
+    try:
+        response = requests.post(url, data=payload)
+        json_data = response.json()
+
+        if json_data:
+            print("[{}] {}".format(json_data['id'], json_data['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
